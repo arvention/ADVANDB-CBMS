881fee568ac01644f645ddd184f630f5a66d56f3
ebackers <arcestalavera@gmail.com>
2016-04-09 00:39:17 +0800

    Marinduque Server

---
+++ [ADVANDB] Marinduque Server/src/marinduque/server/driver/MarinduqueServerDriver.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package marinduque.server.driver;
+
+/**
+ *
+ * @author Arces
+ */
+public class MarinduqueServerDriver {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
