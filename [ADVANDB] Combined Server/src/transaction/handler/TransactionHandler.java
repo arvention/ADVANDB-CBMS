0e896d43b2c8aa2bce0f692155f8c8d730cabcfe
ebackers <arcestalavera@gmail.com>
2016-04-10 21:37:19 +0800

    push

--- [ADVANDB] Combined Server/src/transaction/handler/TransactionHandler.java
+++ [ADVANDB] Combined Server/src/transaction/handler/TransactionHandler.java
@@ -3,29 +3,52 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package transaction.handler;
 
 import GUI.ServerGUI;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Arces
  */
 public class TransactionHandler {
+
     private QueryReceiver receiver;
     private QueryProcessor processor;
     private ServerGUI gui;
+    private int numProcessor;
+    private ArrayList<Thread> processors;
     
-    public TransactionHandler(){
+    public TransactionHandler() {
         this.gui = gui.getInstance();
         
+        this.numProcessor = 8;
+        this.processors = new ArrayList<>();
+        
+        startServer();
+    }
+
+    public void startServer() {
         this.receiver = new QueryReceiver(8);
         Thread r = new Thread(receiver);
         r.start();
+
+        for (int i = 0; i < numProcessor; i++) {
+            this.processor = new QueryProcessor();
+            Thread p = new Thread(processor);
+            processors.add(p);
+            p.start();
+        }
         
-        this.processor = new QueryProcessor();
-        Thread p = new Thread(processor);
-        p.start();
+        for(Thread p: processors){
+            try {
+                p.join();
+            } catch (InterruptedException ex) {
+                Logger.getLogger(TransactionHandler.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 }
